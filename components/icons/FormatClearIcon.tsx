1f85e5708e75b242760f4965281da10b4f8960e2
wallacejs2 <wallacejs4@outlook.com>
2025-09-15 10:21:08 -0500

    feat(rich-text): Add rich text editing capabilities
    Introduce basic rich text editing functionality to the KnowledgeBaseView. This includes:
    - Basic HTML tag support in the styling.
    - New icons for formatting options like headings, text color, and highlighter.
    - Enhanced RichTextEditor component with heading selection and placeholder visibility.

---
+++ components/icons/FormatClearIcon.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const FormatClearIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" fill="currentColor" {...props}>
+    <path d="M0 0h24v24H0z" fill="none"/>
+    <path d="M3.27 5L2 6.27l6.97 6.97L6.5 19h3l1.57-3.66L16.73 21 18 19.73 3.27 5zM6 10l-2.8-7h13.3l-1.9 4.5z"/>
+  </svg>
+);
---
+++ components/icons/HighlighterIcon.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+export const HighlighterIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props}>
+    <path d="m15.82 17.5-3.32-3.32 1.41-1.42 3.32 3.32-1.41 1.42ZM20.41 6c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0L16 2.92 19.08 6l1.33-1.41v.01zm-3.33 1.41L3 21.5V17h4.76L17.08 7.41zM5.5 19H5v-.5l.25-.25L9 14.5l.5.5-3.75 3.75z"/>
+  </svg>
+);
